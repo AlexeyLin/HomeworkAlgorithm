5613b69e2595071d7b420718cf5b3881018a752f
AlexeyLin <aleksey.lin@gmail.com>
2021-02-22 23:23:53 +0300

    Dz1 done

---
+++ Dz1/Project1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        public class TestCase
+        {
+            public int input { get; set; }
+            public string expectation { get; set; }
+        }
+
+        static string IsPrimeNum (int n)
+        {
+            int d = 0;
+            int i = 2;
+            while (i<n)
+            {
+                if (n % i == 0) d++;
+                i++;
+            }
+            if (d==0)
+            {
+                return "Простое";
+            }
+            else
+            {
+                return "Не простое";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var casesTest = new TestCase[]
+            {
+                new TestCase{input = 16, expectation = "Не простое" },
+                new TestCase{input = 7, expectation = "Простое" },
+                new TestCase{input = 23, expectation = "Простое" },
+            };
+            foreach (var TestCase in casesTest)
+            {
+                Console.WriteLine($"Входное значение: {TestCase.input}\n" +
+                $"Ожидаемый результат: {TestCase.expectation}\n" +
+                $"Результат работы функции: {IsPrimeNum(TestCase.input)}\n");
+            }
+            IsPrimeNum(4);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dz1/Project2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; // Сложность операции O(1)
+            for (int i = 0; i < inputArray.Length; i++) // Сложность цикла O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) // Сложность цикла O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // Сложность цикла O(N), так как сложностью операций внутри цикла можно пренебречь
+                    {
+                        int y = 0; // Сложность операции O(1)
+
+                        if (j != 0) // Сложность операции O(1)
+                        {
+                            y = k / j; 
+                        }
+
+                        sum += inputArray[i] + i + k + j + y; // Сложность операции O(1)
+                    }
+                }
+            }
+
+            return sum; // Сложность операции O(1)
+
+            // Итоговая сложность функции O(N*N*N) 
+        }
+    }
+}
