ee1743aca2cc8338a2eecd0341c6450e98ba58d0
AlexeyLin <aleksey.lin@gmail.com>
2021-03-09 18:16:01 +0300

    Dz4 almost done

---
+++ Dz4/Project2/BinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2
+{
+    public class BinaryTree : ITree
+    {
+        static TreeNode rootNode;
+
+        public void AddItem(int value)
+        {
+            TreeNode newNode = null;
+            if (rootNode == null)
+            {
+                rootNode = new TreeNode() { Value = value };
+                return;
+            }
+            newNode = rootNode;
+            while (newNode != null)
+            {
+                if (value > newNode.Value)
+                {
+                    if (newNode.RightChild != null)
+                    {
+                        newNode = newNode.RightChild;
+                        continue;
+                    }
+                    else
+                    {
+                        newNode.RightChild = new TreeNode() { Value = value };
+                        return;
+                    }
+                }
+                else if (value < newNode.Value)
+                {
+                    if (newNode.LeftChild != null)
+                    {
+                        newNode = newNode.LeftChild;
+                        continue;
+                    }
+                    else
+                    {
+                        newNode.LeftChild = new TreeNode() { Value = value };
+                        return;
+                    }
+                }
+            }
+        }
+
+        public TreeNode GetRoot()
+        {
+            return rootNode;
+        }
+
+        public void RemoveItem(int value)
+        {
+            
+        }
+
+        public TreeNode GetNodeByValue(int value)
+        {
+            TreeNode findTree = rootNode;
+            while (findTree.Value != value)
+            {
+                if (value > findTree.Value)
+                {
+                    findTree = findTree.RightChild;
+                    continue;
+                }
+                else if (value < findTree.Value)
+                {
+                    findTree = findTree.LeftChild;
+                    continue;
+                }
+            }
+            return findTree;
+        }
+
+        public void PrintTree()
+        {
+            return;
+        }
+    }
+}
---
+++ Dz4/Project2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newTree = new BinaryTree();
+            newTree.AddItem(2);
+            newTree.AddItem(4);
+            newTree.AddItem(6);
+            newTree.AddItem(1);
+
+            TreeNode testFindNode = newTree.GetNodeByValue(4);
+
+        }
+    }
+}
