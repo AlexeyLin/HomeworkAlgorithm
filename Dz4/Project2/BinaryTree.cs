277546ba6375a0656ee6b7ad0251663d52e60b5c
AlexeyLin <aleksey.lin@gmail.com>
2021-03-15 02:02:52 +0300

    Dz4 done

--- Dz4/Project2/BinaryTree.cs
+++ Dz4/Project2/BinaryTree.cs
@@ -6,17 +6,18 @@ namespace Project2
 {
     public class BinaryTree : ITree
     {
-        static TreeNode rootNode;
+        private TreeNode rootNode;
+        
+
 
         public void AddItem(int value)
         {
-            TreeNode newNode = null;
             if (rootNode == null)
             {
                 rootNode = new TreeNode() { Value = value };
                 return;
             }
-            newNode = rootNode;
+            TreeNode newNode = rootNode;
             while (newNode != null)
             {
                 if (value > newNode.Value)
@@ -32,7 +33,7 @@ namespace Project2
                         return;
                     }
                 }
-                else if (value < newNode.Value)
+                else if (value <= newNode.Value)
                 {
                     if (newNode.LeftChild != null)
                     {
@@ -55,7 +56,73 @@ namespace Project2
 
         public void RemoveItem(int value)
         {
-            
+            TreeNode delTreeNode = rootNode;
+            TreeNode parentNode = new TreeNode();
+            while (delTreeNode.Value != value)
+            {
+                if (value > delTreeNode.Value)
+                {
+                    parentNode = delTreeNode;
+                    delTreeNode = delTreeNode.RightChild;
+                    continue;
+                }
+                else if (value <= delTreeNode.Value)
+                {
+                    parentNode = delTreeNode;
+                    delTreeNode = delTreeNode.LeftChild;
+                    continue;
+                }
+            }
+            if (delTreeNode.LeftChild == null && delTreeNode.RightChild == null)
+            {
+                if (parentNode.LeftChild == delTreeNode)
+                    parentNode.LeftChild = null;
+                else
+                    parentNode.RightChild = null;
+            }
+            else if (delTreeNode.LeftChild == null || delTreeNode.RightChild == null)
+            {
+                if (delTreeNode.LeftChild == null)
+                {
+                    if (parentNode.LeftChild == delTreeNode)
+                        parentNode.LeftChild = delTreeNode.RightChild;
+                    else
+                        parentNode.RightChild = delTreeNode.RightChild;
+                }
+                else
+                {
+                    if (parentNode.LeftChild == delTreeNode)
+                        parentNode.LeftChild = delTreeNode.LeftChild;
+                    else
+                        parentNode.RightChild = delTreeNode.LeftChild;
+                }
+            }
+            else
+            {
+                TreeNode current = delTreeNode;
+                TreeNode replace = current;
+                TreeNode parentReplace = parentNode;
+                while (current != null)
+                {
+                    if (current.Value <= value)
+                    {
+                        parentReplace = parentNode;
+                        replace = current;
+                        parentNode = current;
+                        current = current.LeftChild;
+                    }
+                    else
+                    {
+                        parentNode = current;
+                        current = current.RightChild;
+                    }
+                }
+                delTreeNode.Value = replace.Value;
+                if (parentReplace.LeftChild == replace)
+                    parentReplace.LeftChild = replace.RightChild;
+                else
+                    parentReplace.RightChild = replace.RightChild;
+            }
         }
 
         public TreeNode GetNodeByValue(int value)
@@ -77,9 +144,40 @@ namespace Project2
             return findTree;
         }
 
+        private int cnt;
+
+        public void PrintTreenode(TreeNode node)
+        {           
+            Console.WriteLine(node.Value);
+            if (node.LeftChild != null)
+            {
+                for (int i = 0; i < cnt; i++)
+                {
+                    Console.Write("      ");
+                }
+                Console.Write("|_____");
+                cnt++;
+                PrintTreenode(node.LeftChild);
+                cnt--;
+            }
+            if (node.RightChild != null)
+            {
+                for (int i = 0; i < cnt; i++)
+                {
+                    Console.Write("      ");
+                }
+                Console.Write("|_____");
+                cnt++;
+                PrintTreenode(node.RightChild);
+                cnt--;
+            }
+            return;
+            
+        }
+
         public void PrintTree()
         {
-            return;
+            PrintTreenode(rootNode);
         }
     }
 }
--- Dz4/Project2/Program.cs
+++ Dz4/Project2/Program.cs
@@ -7,14 +7,43 @@ namespace Project2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Тест создания и заполнения дерева данными:");
+            Console.WriteLine("На вход подаются цифры: 10 4 6 1 13 7 11 12 14.");
+            Console.WriteLine("Результат работы программы:");
             var newTree = new BinaryTree();
-            newTree.AddItem(2);
+            newTree.AddItem(10);
             newTree.AddItem(4);
             newTree.AddItem(6);
             newTree.AddItem(1);
+            newTree.AddItem(13);
+            newTree.AddItem(7);
+            newTree.AddItem(11);
+            newTree.AddItem(12);
+            newTree.AddItem(14);
+            newTree.PrintTree();
+            Console.WriteLine();
 
-            TreeNode testFindNode = newTree.GetNodeByValue(4);
+            Console.WriteLine("Тест удаления узла дерева:");
+            Console.WriteLine("Удаление 14 без листьев ");
+            newTree.RemoveItem(14);
+            Console.WriteLine("Результат работы программы:");
+            newTree.PrintTree();
+            Console.WriteLine();
 
+            Console.WriteLine("Удаление 11 с 1 листом");
+            newTree.RemoveItem(11);
+            Console.WriteLine("Результат работы программы:");
+            newTree.PrintTree();
+            Console.WriteLine();
+
+            Console.WriteLine("Удаление 4 с 2 листами");
+            newTree.RemoveItem(4);
+            Console.WriteLine("Результат работы программы:");
+            newTree.PrintTree();
+            Console.WriteLine();
+
+            NodeInfo[] test = TreeHelper.GetTreeInLine(newTree);
+            Console.Read();
         }
     }
 }
