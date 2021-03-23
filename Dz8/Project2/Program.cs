9a308a17c4db0a4d6776dd39914033c6e56f7341
AlexeyLin <aleksey.lin@gmail.com>
2021-03-23 19:10:29 +0300

    Start project2

--- Dz8/Project2/Program.cs
+++ Dz8/Project2/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Project2
 {
     class Program
     {
+        static int[] BucketSort(int[] array)
+        {
+            int cnt = 0;
+            int minNumArray = array[0];
+            int maxNumArray = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                minNumArray = Math.Min(minNumArray, array[i]);
+                maxNumArray = Math.Max(maxNumArray, array[i]);
+            }
+            int rangeNum = maxNumArray - minNumArray;
+            int numBucket = rangeNum / 20;
+            List<int>[] arrayBucket = new List<int>[numBucket];
+            for (int i = 0; i < arrayBucket.Length; i++)
+                arrayBucket[i] = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int index = array[i] * (numBucket - 1) / rangeNum;
+                arrayBucket[index].Add(array[i]);
+            }
+            for (int i = 0; i < arrayBucket.Length; i++)
+            {
+                arrayBucket[i].Sort();
+                foreach (int num in arrayBucket[i])
+                {
+                    array[cnt] = num;
+                    cnt++;
+                }
+            }
+            return array;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Создание папки для хранения файлов и файла с начальными данными
+            Random rnd = new Random();
+            string path = @"C:\testfile";
+            DirectoryInfo di = Directory.CreateDirectory(path);
+            string initialDataPath = @"C:\testfile\initialData.txt";
+            using (StreamWriter initialData = File.CreateText(initialDataPath))
+            {
+                for(int i = 0;i<100000; i++)
+                {
+                    initialData.WriteLine(rnd.Next(1, 1000));
+                }
+            }
+
         }
     }
 }
