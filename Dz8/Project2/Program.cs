d721dd495b0f2e15bfbc54c86054a9068ef145de
AlexeyLin <aleksey.lin@gmail.com>
2021-03-29 01:19:26 +0300

    Finish version

--- Dz8/Project2/Program.cs
+++ Dz8/Project2/Program.cs
@@ -6,55 +6,8 @@ namespace Project2
 {
     class Program
     {
-        static int[] BucketSort(int[] array)
+        static void ExeternalSort(string initialDataPath)
         {
-            int cnt = 0;
-            int minNumArray = array[0];
-            int maxNumArray = array[0];
-            for (int i = 0; i < array.Length; i++)
-            {
-                minNumArray = Math.Min(minNumArray, array[i]);
-                maxNumArray = Math.Max(maxNumArray, array[i]);
-            }
-            int rangeNum = maxNumArray - minNumArray;
-            int numBucket = rangeNum / 20;
-            List<int>[] arrayBucket = new List<int>[numBucket];
-            for (int i = 0; i < arrayBucket.Length; i++)
-                arrayBucket[i] = new List<int>();
-            for (int i = 0; i < array.Length; i++)
-            {
-                int index = array[i] * (numBucket - 1) / rangeNum;
-                arrayBucket[index].Add(array[i]);
-            }
-            for (int i = 0; i < arrayBucket.Length; i++)
-            {
-                arrayBucket[i].Sort();
-                foreach (int num in arrayBucket[i])
-                {
-                    array[cnt] = num;
-                    cnt++;
-                }
-            }
-            return array;
-        }
-
-        static void Main(string[] args)
-        {
-            //Создание папки для хранения файлов и файла с начальными данными
-            Random rnd = new Random();
-            string path = @"C:\testfile";
-            DirectoryInfo di = Directory.CreateDirectory(path);
-            string initialDataPath = @"C:\testfile\initialData.txt";
-            using (StreamWriter initialData = File.CreateText(initialDataPath))
-            {
-                for(int i = 0;i<105; i++)
-                {
-                    initialData.WriteLine(rnd.Next(1, 1000));
-                }
-            }
-
-
-
             int cnt = 1;
             using (StreamReader readData = new StreamReader(initialDataPath))
             {
@@ -62,7 +15,7 @@ namespace Project2
                 while (number != 0)
                 {
                     List<int> array = new List<int>();
-                    for (int i = 0; i < 10; i++)
+                    for (int i = 0; i < 100000; i++)
                     {
                         array.Add(number);
                         number = Convert.ToInt32(readData.ReadLine());
@@ -76,20 +29,74 @@ namespace Project2
                     {
                         for (int i = 0; i < array.Count; i++)
                         {
-                            initialData.WriteLine(Convert.ToString(array[i]));                          
+                            initialData.WriteLine(Convert.ToString(array[i]));
                         }
-                    } 
+                    }
                     cnt++;
                 }
             }
+            File.Delete(initialDataPath);
+            int fileNum = 1;
+            while (File.Exists($"C:\\testfile\\{fileNum}.txt"))
+            {
+                if (!File.Exists($"C:\\testfile\\{fileNum + 1}.txt"))
+                {
+                    File.Copy($"C:\\testfile\\{fileNum}.txt", initialDataPath);
+                    File.Delete($"C:\\testfile\\{fileNum}.txt");
+                }
+                else
+                {
+                    using (StreamReader readDataOne = new StreamReader($"C:\\testfile\\{fileNum}.txt"))
+                    {
+                        int numOne = Convert.ToInt32(readDataOne.ReadLine());
+                        using (StreamReader readDataTwo = new StreamReader($"C:\\testfile\\{fileNum + 1}.txt"))
+                        {
 
-            Console.WriteLine();
+                            int numTwo = Convert.ToInt32(readDataTwo.ReadLine());
+                            using (StreamWriter initialDataCurrent = File.CreateText($"C:\\testfile\\{cnt}.txt"))
+                            {
+                                string numMin = "";
+                                do
+                                {
 
+                                    if ((numOne <= numTwo && numOne != 0) || (numTwo == 0 && numOne != 0))
+                                    {
+                                        numMin = Convert.ToString(numOne);
+                                        numOne = Convert.ToInt32(readDataOne.ReadLine());
+                                    }
+                                    else if ((numTwo != 0 && numOne > numTwo) || (numTwo != 0 && numOne == 0))
+                                    {
+                                        numMin = Convert.ToString(numTwo);
+                                        numTwo = Convert.ToInt32(readDataTwo.ReadLine());
+                                    }
+                                    initialDataCurrent.WriteLine(numMin);
+                                } while (numOne != 0 || numTwo != 0);
+                            }
+                            cnt++;
+                        }
+                    }
+                    File.Delete($"C:\\testfile\\{fileNum}.txt");
+                    File.Delete($"C:\\testfile\\{fileNum + 1}.txt");
+                    fileNum += 2;
+                }
+            }
+        }
 
-
-
-
-
+        static void Main(string[] args)
+        {
+            //Создание папки для хранения файлов и файла с начальными данными
+            Random rnd = new Random();
+            string path = @"C:\testfile";
+            DirectoryInfo di = Directory.CreateDirectory(path);
+            string initialDataPath = @"C:\testfile\initialData.txt";
+            using (StreamWriter initialData = File.CreateText(initialDataPath))
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    initialData.WriteLine(rnd.Next(1, 1000));
+                }
+            }
+            ExeternalSort(initialDataPath);
         }
     }
 }
